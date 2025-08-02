955e6ada4786ee3ecba14bd239d2feda30987b35
yn-wy <otakuynwy999@gmail.com>
2025-08-03 00:56:54 +0800

    Modify lecture 1 exercise(s) code to be consistent

--- Lecture1/Exercise1.0/Program.cs
+++ Lecture1/Exercise1.0/Program.cs
@@ -1,68 +1,67 @@
-﻿namespace Exercise1._0
+﻿namespace Exercise1._0;
+
+abstract class Vehicle
 {
-    abstract class Vehicle
+    private string make;
+    private string model;
+    private int year;
+
+    public Vehicle(string make, string model, int year)
     {
-        private string make;
-        private string model;
-        private int year;
-
-        public Vehicle(string make, string model, int year)
-        {
-            this.make = make;
-            this.model = model;
-            this.year = year;
-        }
-
-        public virtual void vehicle_info()
-        {
-            Console.WriteLine("#########################");
-            Console.WriteLine($"Make:  {make}");
-            Console.WriteLine($"Model: {model}");
-            Console.WriteLine($"Year:  {year}");
-        }
+        this.make = make;
+        this.model = model;
+        this.year = year;
     }
 
-    class Car : Vehicle
+    public virtual void vehicle_info()
     {
-        private int num_doors;
+        Console.WriteLine("#########################");
+        Console.WriteLine($"Make:  {make}");
+        Console.WriteLine($"Model: {model}");
+        Console.WriteLine($"Year:  {year}");
+    }
+}
 
-        public Car(string make, string model, int year, int num_doors) : base(make, model, year)
-        {
-            this.num_doors = num_doors;
-        }
+class Car : Vehicle
+{
+    private int num_doors;
 
-        public override void vehicle_info()
-        {
-            base.vehicle_info();
-            Console.WriteLine($"Number of doors: {num_doors}");
-        }
+    public Car(string make, string model, int year, int num_doors) : base(make, model, year)
+    {
+        this.num_doors = num_doors;
     }
 
-    class Bike : Vehicle
+    public override void vehicle_info()
     {
-        private string bike_type;
+        base.vehicle_info();
+        Console.WriteLine($"Number of doors: {num_doors}");
+    }
+}
 
-        public Bike(string make, string model, int year, string bike_type) : base(make, model, year)
-        {
-            this.bike_type = bike_type;
-        }
+class Bike : Vehicle
+{
+    private string bike_type;
 
-        public override void vehicle_info()
-        {
-            base.vehicle_info();
-            Console.WriteLine($"Bike type: {bike_type}");
-        }
+    public Bike(string make, string model, int year, string bike_type) : base(make, model, year)
+    {
+        this.bike_type = bike_type;
     }
 
-    internal class Program
+    public override void vehicle_info()
     {
-        static void Main(string[] args)
-        {
-            Car car = new Car("Toyota", "GR Corolla", 2025, 5);
-            Bike bike = new Bike("Canyon", "Aeroad CFR Di2", 2024, "Road Bike");
+        base.vehicle_info();
+        Console.WriteLine($"Bike type: {bike_type}");
+    }
+}
 
-            car.vehicle_info();
-            bike.vehicle_info();
-        }
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car("Toyota", "GR Corolla", 2025, 5);
+        Bike bike = new Bike("Canyon", "Aeroad CFR Di2", 2024, "Road Bike");
+
+        car.vehicle_info();
+        bike.vehicle_info();
     }
 }
